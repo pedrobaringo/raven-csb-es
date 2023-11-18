969689d45c7d4451a61d91bfa537188425d13b6a
pedrobaringo <pedro.baringo@gmail.com>
2023-11-18 11:21:59 +0100

    1.0.0

---
+++ modules/die.js
@@ -0,0 +1,45 @@
+export class RavenCorvus extends Die {
+    constructor(termData) {
+        termData.faces=6;
+        super(termData);
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    static DENOMINATION = "v";
+     /** @override */
+    getResultLabel(result) {
+        return {
+			"1": '<img src="modules/raven-csb/images/CaraBlankGato_inCHAT.png" />',
+            "2": '<img src="modules/raven-csb/images/CaraBlankGato_inCHAT.png" />',
+            "3": '<img src="modules/raven-csb/images/CaraBlankGato_inCHAT.png" />',
+            "4": '<img src="modules/raven-csb/images/CaraGato_inCHAT.png" />',
+			"5": '<img src="modules/raven-csb/images/CaraGato_inCHAT.png" />',			
+            "6": '<img src="modules/raven-csb/images/CaraGato_inCHAT.png" />'
+        }[result.result];
+    }
+}
+
+export class RavenNiebla extends Die {
+    constructor(termData) {
+        termData.faces=6;
+        super(termData);
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    static DENOMINATION = "n";
+     /** @override */
+    getResultLabel(result) {
+        return {
+			"1": '<img src="modules/raven-csb/images/CaraBlankCuervo_inCHAT.png" />',
+            "2": '<img src="modules/raven-csb/images/CaraBlankCuervo_inCHAT.png" />',
+            "3": '<img src="modules/raven-csb/images/CaraBlankCuervo_inCHAT.png" />',
+            "4": '<img src="modules/raven-csb/images/CaraCuervo_inCHAT.png" />',
+			"5": '<img src="modules/raven-csb/images/CaraCuervo_inCHAT.png" />',			
+            "6": '<img src="modules/raven-csb/images/CaraCuervo_inCHAT.png" />'
+        }[result.result];
+    }
+}
---
+++ modules/raven.js
@@ -0,0 +1,73 @@
+import {RavenCorvus} from './die.js';
+import {RavenNiebla} from './die.js';
+
+Hooks.once("init", async function () {
+    CONFIG.Dice.terms["v"] = RavenCorvus;
+	CONFIG.Dice.terms["n"] = RavenNiebla;
+});
+
+Hooks.once('diceSoNiceReady', (dice3d) => {
+    dice3d.addSystem({id:"ravenDice",name:"Raven Dice"},"default");
+    dice3d.addDicePreset({
+      type:"dv",
+      labels:[
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraGato.png',
+        'modules/raven-csb/images/CaraGato.png', 
+        'modules/raven-csb/images/CaraGato.png'
+      ],
+      bumpMaps:[
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraGato_Bump.png',
+        'modules/raven-csb/images/CaraGato_Bump.png', 
+        'modules/raven-csb/images/CaraGato_Bump.png'
+      ],
+	  colorset:"blackCorvus",
+      system: "ravenDice"
+    });
+	dice3d.addDicePreset({
+      type:"dn",
+      labels:[
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraBlank.png', 
+        'modules/raven-csb/images/CaraCuervo.png',
+        'modules/raven-csb/images/CaraCuervo.png', 
+        'modules/raven-csb/images/CaraCuervo.png'
+      ],
+      bumpMaps:[
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraBlank_Bump.png', 
+        'modules/raven-csb/images/CaraCuervo_Bump.png',
+        'modules/raven-csb/images/CaraCuervo_Bump.png', 
+        'modules/raven-csb/images/CaraCuervo_Bump.png'
+      ],
+	  colorset:"whiteCorvus",
+      system: "ravenDice"
+    });
+dice3d.addColorset({
+    name: 'whiteCorvus',
+    description: 'White',
+    category: 'Raven Dice',
+    foreground: '#FFFFFF',
+    background: '#FFFFFF',
+    outline: 'white',
+    texture: 'none',
+    material: 'plastic'
+});
+dice3d.addColorset({
+    name: 'blackCorvus',
+    description: 'Black',
+    category: 'Raven Dice',
+    foreground: '#000000',
+    background: '#000000',
+    outline: 'black',
+    texture: 'none',
+    material: 'plastic'
+});
+});
